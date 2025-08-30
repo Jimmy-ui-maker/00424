c3d2398d7e4dcd99706f3b804c14f9a9bcda4c30
Jimmy Yakubu <jimiyaks3@gmail.com>
2025-08-30 15:36:22 +0100

    shadow fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,17 @@ export default function Footer() {
     <footer className="py-4 mt-5">
       <div className="container">
         <div className="soft-card p-3 d-flex flex-column flex-md-row justify-content-between align-items-center">
+          {/* Left: Logo */}
+          <div>
+            <a className="navbar-brand fw-bold" href="/">
+              Sir Jimmy
+            </a>
+          </div>
+
           <div className="small">© {year} Jimi Yaks. All rights reserved.</div>
           <div className="small d-flex gap-3">
-            <a href="mailto:jimiyaks3@gmail.com" target="_blank">
-              Email
-            </a>
             <a href="https://wa.me/2347010228016" target="_blank">
-              WhatsApp
+              <i className=" bi bi-whatsapp text-success px-3 mt-sm-4"></i>
             </a>
           </div>
         </div>
